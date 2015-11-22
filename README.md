9df01350ea3b6efde299b886387ead303e269e2e
Matthieu Monsch <monsch@alum.mit.edu>
2015-11-22 14:33:36 -0800

    Add a few tests.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Pal [![Build status](https://travis-ci.org/mtth/pal.svg?branch=master)](https://travis-ci.org/mtth/pal)
 
-JS/C++ version of PalDB.
+Key-value store inspired by [PalDB](https://github.com/linkedin/PalDB).
---
+++ test/test_index.js
@@ -0,0 +1,58 @@
+/* jshint node: true, mocha: true */
+
+'use strict';
+
+var pal = require('../lib'),
+    assert = require('assert');
+
+suite('index', function () {
+
+  suite('store', function () {
+
+    var store = new pal.Store('test/dat/numbers.store');
+
+    test('get missing', function (done) {
+      store.get(new Buffer([]), function (err, data) {
+        assert(err);
+        assert.strictEqual(data, undefined);
+        done();
+      });
+    });
+
+    test('get existing', function (done) {
+      store.get(new Buffer([0x67, 0x03, 0x6f, 0x6e, 0x65]), function (err, buf) {
+        assert(!err);
+        assert.deepEqual(buf, new Buffer([0x06]));
+        done();
+      });
+    });
+
+    test('createReadStream', function (done) {
+      var entries = [];
+      store.createReadStream()
+        .on('data', function (entry) { entries.push(entry); })
+        .on('end', function () {
+          assert.deepEqual(
+            entries,
+            [
+              {
+                key: new Buffer([0x67, 0x03, 0x6f, 0x6e, 0x65]),
+                value: new Buffer([0x06])
+              },
+              {
+                key: new Buffer([0x67, 0x03, 0x74, 0x77, 0x6f]),
+                value: new Buffer([0x07])
+              },
+              {
+                key: new Buffer([0x67, 0x05, 0x74, 0x68, 0x72, 0x65, 0x65]),
+                value: new Buffer([0x08])
+              },
+            ]
+          );
+          done();
+        });
+    });
+
+  });
+
+});
